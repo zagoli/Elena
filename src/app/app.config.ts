bec1db7bdd0ac2e195a5c9661565181c1d822e44
jacopo <jacopo@zagoli.com>
2024-03-19 16:31:41 +0100

    Default locale

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
-import {ApplicationConfig} from '@angular/core';
+import {ApplicationConfig, LOCALE_ID} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
-		provideRouter(routes)
+		provideRouter(routes),
+		{provide: LOCALE_ID, useValue: 'it-IT'}
 	]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 /// <reference types="@angular/localize" />
-import {registerLocaleData} from '@angular/common';
-import localeIt from '@angular/common/locales/it';
 import {bootstrapApplication} from '@angular/platform-browser';
 import {appConfig} from './app/app.config';
 import {AppComponent} from './app/app.component';
+import {registerLocaleData} from '@angular/common';
+import localeIt from '@angular/common/locales/it';
 
 registerLocaleData(localeIt);
 
