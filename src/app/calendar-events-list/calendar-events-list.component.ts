a53aa48f575b658875c4eeec27284f0d82d729e7
jacopo <jacopo@zagoli.com>
2024-03-20 15:57:10 +0100

    recurrent events

--- src/app/calendar-events-list/calendar-events-list.component.ts
+++ src/app/calendar-events-list/calendar-events-list.component.ts
@@ -3,6 +3,8 @@ import {CalendarEventsService} from "../calendar-events.service";
 import {FormsModule} from "@angular/forms";
 import {CalendarEvent} from "../types/CalendarEvent"
 import {CalendarEventComponent} from "../calendar-event/calendar-event.component";
+// @ts-ignore
+import * as ICAL from 'ical.js';
 
 @Component({
 	selector: 'app-calendar-events-list',
@@ -19,19 +21,27 @@ export class CalendarEventsListComponent {
 	urlSet = false;
 	todayEvents: CalendarEvent[] = [];
 	calendarUrl: string = "";
-	private today = new Date(2023, 0, 1);
+	private today = new Date();
+	private updateFrequencySeconds = 3600;
 
 	constructor(private calendarEventsService: CalendarEventsService) {
 	}
 
-	// https://ics.calendarlabs.com/53/556c8add/Italy_Holidays.ics
+	startUpdates() {
+		this.getEvents();
+		setInterval(() => {
+			this.today = new Date();
+			this.todayEvents = [];
+			this.getEvents();
+		}, 1000 * this.updateFrequencySeconds);
+	}
 
 	getEvents(): void {
 		this.calendarEventsService.getCalendarFile(this.calendarUrl)
 			.subscribe(icsEvents => {
 				const calendarEvents = this.icsEventsToCalendarEvents(icsEvents);
 				this.todayEvents = this.todayEvents.concat(this.getEventsHappeningToday(calendarEvents));
-				console.log(this.getRecurrentEventsHappeningToday(icsEvents));
+				this.todayEvents = this.todayEvents.concat(this.getRecurrentEventsHappeningToday(icsEvents));
 			});
 		this.urlSet = true;
 	}
@@ -56,10 +66,34 @@ export class CalendarEventsListComponent {
 	}
 
 	private getRecurrentEventsHappeningToday(events: any[]): CalendarEvent[] {
-		const recurrentPastEvents = events.filter((e: any) => {
+		let recurrentPastEvents = events.filter((e: any) => {
 			return e.isRecurring() && e.startDate.toJSDate() <= this.today;
 		});
+		recurrentPastEvents = recurrentPastEvents.filter((e: any) => {
+			return this.doesRecurrentPastEventHappenToday(e);
+		});
 		return this.icsEventsToCalendarEvents(recurrentPastEvents);
+	}
+
+	private doesRecurrentPastEventHappenToday(event: any): boolean {
+		const expand = new ICAL.RecurExpansion({
+			component: event.component,
+			dtstart: event.startDate
+		});
+
+		let next = expand.next();
+		do {
+			if (this.datesEquals(next.toJSDate(), this.today))
+				return true;
+		} while ((next = expand.next()) && next.toJSDate() <= this.today);
+
+		return false;
+	}
+
+	private datesEquals(date1: Date, date2: Date): boolean {
+		date1.setHours(0, 0, 0, 0);
+		date2.setHours(0, 0, 0, 0);
+		return date1.getTime() === date2.getTime();
 	}
 
 }
