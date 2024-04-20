abb0a1704c7da0d684190c8f35c92bf8882b9794
jacopo <jacopo@zagoli.com>
2024-04-20 14:44:48 +0200

    toggle fullscreen

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ Elena is a straightforward web application designed as a calendar specifically t
 I made it to revise my basic Angular skills.
 It features a minimalistic interface that allows users
 to insert a link to a public calendar (ICS file) to display
-the events of the current day (in landascape orientation).
+the events of the current day (in landscape mode otherwise it is even uglier).
 The event list refreshes every 30 minutes.
+Click on the date to toggle fullscreen mode.
 
 For a live demonstration of the Elena Calendar, please visit [elena.zagoli.com](elena.zagoli.com).
 
--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -14,13 +14,26 @@ import {DatePipe, TitleCasePipe} from "@angular/common";
 export class CalendarComponent implements OnInit {
 	date: Date = new Date();
 	private updateDateFrequencyMilliseconds = 100;
+	protected readonly document = document;
 
 	ngOnInit(): void {
 		this.updateDate();
-		setInterval(() => {this.updateDate()}, this.updateDateFrequencyMilliseconds);
+		setInterval(() => {
+			this.updateDate()
+		}, this.updateDateFrequencyMilliseconds);
 	}
 
 	private updateDate(): void {
 		this.date = new Date();
+	}
+
+	toggleFullscreen(): void {
+		if (document.fullscreenEnabled) {
+			if (document.fullscreenElement != null) {
+				void document.exitFullscreen();
+			} else {
+				void document.documentElement.requestFullscreen();
+			}
+		}
 	}
 }
